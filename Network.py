3a73c3ecf3b97b2d8423b72bfb1bd8d46d60648b
William Savage <william_savage@bose.com>
2019-10-22 13:55:09 -0400

    Added Comments to the Network, and added batch support

--- Network.py
+++ Network.py
@@ -1,29 +1,34 @@
+from typing import Tuple
+
 import NeatLinearNet
 import Simulation
 
 
 class Network:
-    def __init__(self, weight_matrix, enabled_matrix, input_size, output_size, middle_size, cache_size=0):
+    def __init__(self, weight_matrix, enabled_matrix, input_size, output_size, middle_size, cache_size=0, batch_id=None):
         """
-
-        :param weight_matrix:
-        :param enabled_matrix:
-        :param input_size:
-        :param output_size:
-        :param middle_size:
-        :param cache_size:
+        The Network represents a Neural Network
+        Can have a cache to save answers
+        :param weight_matrix: The matrix representing the weights of the neural network
+        :param enabled_matrix: The matrix showing which connections exist in the neural network
+        :param input_size: The number of input nodes to the network
+        :param output_size: The number of output nodes from the network
+        :param middle_size: The number of hidden nodes in the network
+        :param cache_size: The size, in cache entries, of the cache for saving answers
         """
         self.neural_net: NeatLinearNet = NeatLinearNet.NeatLinearNet(input_size, output_size, middle_size,
-                                                                     weight_matrix,
-                                                                     enabled_matrix)
+                                                                     weight_matrix, enabled_matrix)
         self.cache_size = cache_size
         self.cache = {}
+        self.batch_id = batch_id
 
-    def run(self, input_values: tuple[float]) -> tuple[float]:
+    def run(self, input_values: Tuple[float]) -> Tuple[float]:
         """
-
-        :param input_values:
-        :return:
+        Runs the neural network on an input and returns the output
+        If the cache exists, it checks if the input is in the cache
+        If using a cache, caches the output
+        :param input_values: The values to use as input to the network
+        :return: The values from the output layer of the neural network
         """
         if input_values in self.cache:
             out = self.cache[input_values]
@@ -42,12 +47,19 @@ class Network:
 
     def execute(self, simulation: Simulation):
         """
-
-        :param simulation:
-        :return:
+        Runs a simulation using the using the neural network as the agent
+        :param simulation: The simulation to run, and get the score from
+        :return: The score from the simulation
         """
-        while simulation.get_state() != Simulation.SimulationState.FINISHED:
-            input_data = simulation.get_data()
+        while simulation.get_state(batch_id=self.batch_id) != Simulation.SimulationState.FINISHED:
+            input_data = simulation.get_data(batch_id=self.batch_id)
             processed_data = self.run(input_data)
-            simulation.apply_input(processed_data)
+            simulation.apply_controls(processed_data, batch_id=self.batch_id)
         return simulation.get_score()
+
+    def set_batch_id(self, batch_id):
+        """
+        Sets the batch id of the network
+        :param batch_id: The batch id to change the current batch id to
+        """
+        self.batch_id = batch_id
