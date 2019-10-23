35dee57fad499f5b6ec331dedd7137fce652dd43
William Savage <william_savage@bose.com>
2019-10-23 16:44:58 -0400

    Added gene to adjacency matrix function

--- Genome.py
+++ Genome.py
@@ -8,19 +8,42 @@ from Conditions import Conditions
 import numpy as np
 
 
-def make_adjacencys(genes: List[Gene], input_size: int, output_size: int) -> Tuple[np.array, np.array]:
-    return np.array([]), np.array([])
+def genes_to_matrices_and_dim(genes: List[Gene], input_size: int, output_size: int) -> Tuple[np.array, np.array, int]:
+    nodes = set()
+    middles = set()
+    for connection_gene in genes:
+        nodes.add(connection_gene.in_node)
+        nodes.add(connection_gene.out_node)
 
+        if connection_gene.in_node >= input_size:
+            middles.add(connection_gene[1])
 
-def get_middle_size(genes: List[Gene], input_size: int, output_size: int) -> int:
-    return 0
+        if connection_gene.out_node > 0:
+            middles.add(connection_gene[2])
+
+    list(map(nodes.add, range(input_size)))
+    list(map(nodes.add, range(-output_size, 0)))
+    nodes = list(nodes)
+    nodes.sort()
+
+    middle_size = len(middles)
+    enabled_matrix = np.zeros((input_size + middle_size, middle_size + output_size), dtype=bool)
+    weight_matrix = np.zeros((input_size + middle_size, middle_size + output_size))
+
+    for gene in genes:
+        if gene.enabled:
+            start = nodes.index(gene.in_node) - output_size
+            end = gene.out_node if gene.out_node < 0 else nodes.index(gene.out_node) - (input_size + output_size)
+            enabled_matrix[start][end] = True
+            weight_matrix[start][end] = gene.weight
+    return weight_matrix, enabled_matrix, middle_size
 
 
 class Genome:
     def __init__(self, genes: List[Gene], input_size: int, output_size: int):
         self.genes: List[Gene] = genes  # its is assumed that the genes will be in sorted order
-        weight_matrix, enabled_matrix = make_adjacencys(self.genes, input_size, output_size)
-        middle_size = get_middle_size(self.genes)
+        weight_matrix, enabled_matrix, middle_size = genes_to_matrices_and_dim(self.genes, input_size, output_size)
+
         self.network: Network = Network(weight_matrix, enabled_matrix, input_size, output_size, middle_size)
         self.raw_fitness = 0
 
