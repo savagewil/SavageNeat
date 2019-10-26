af060db4f259397c9978648754e75a80fe514ee0
William Savage <william_savage@bose.com>
2019-10-26 14:32:50 -0400

    Added comparison methods to Genome

--- Genome.py
+++ Genome.py
@@ -183,80 +183,55 @@ class Genome:
 
     def __eq__(self, other: Genome) -> bool:
         """
-        Checks to see if the structure of the gene is the same
-        can be used when finding the innovation number
-        :param other: Another object, should be a Gene
-        :return: True if the other is a Gene with the same structure (in and out nodes), False otherwise
+        Checks to see if the genomes have the same score
+        :param other: Another object, should be a Genome
+        :return: True if the other is a Genome with the same score, False otherwise
         """
         if isinstance(other, Genome):
-            return self.in_node == other.in_node and self.out_node == other.out_node
+            return self.raw_fitness == other.raw_fitness
         else:
             return False
 
     def __lt__(self, other: Genome) -> bool:
         """
-        Checks if the current gene is less than another gene
-        Looks to see if the current gene's in_node is less than the other genes's in node,
-        or the in nodes are equal, and the current gene's out node is less than the other's out node
-        :param other: An object which should represent a Gene
-        :return: True if the structure of the gene is less than the structure of the other, False otherwise
+        Checks to see if the score of the current genome is less than the score of the other
+        :param other: An object which should represent a Genome
+        :return: True if the score of the genome is less than the score of the other, False otherwise
         """
         if isinstance(other, Genome):
-            return self.in_node < other.in_node or (self.in_node == other.in_node and self.out_node < other.out_node)
+            return self.raw_fitness < other.raw_fitness
         else:
-            raise TypeError("Greater than or equal is not supported between %s and %s" % (str(type(self)), str(type(other))))
+            raise TypeError("Less than is not supported between %s and %s" % (str(type(self)), str(type(other))))
 
     def __le__(self, other: Genome) -> bool:
         """
-        Checks if the current gene is less than or equal to another gene
-        Looks to see if the current gene's in_node is less than the other genes's in node,
-        or the in nodes are equal, and the current gene's out node is less than or equal to the other's out node
-        :param other: An object which should represent a Gene
-        :return: True if the structure of the gene is less than or equal to the structure of the other, False otherwise
+        Checks to see if the score of the current genome is less than or equal the score of the other
+        :param other: An object which should represent a Genome
+        :return: True if the score of the genome is less than or equal to the score of the other, False otherwise
         """
         if isinstance(other, Genome):
-            return self.in_node < other.in_node or (self.in_node == other.in_node and self.out_node <= other.out_node)
+            return self.raw_fitness <= other.raw_fitness
         else:
-            raise TypeError("Greater than or equal is not supported between %s and %s" % (str(type(self)), str(type(other))))
+            raise TypeError("Less than or equal is not supported between %s and %s" % (str(type(self)), str(type(other))))
 
     def __gt__(self, other: Genome) -> bool:
         """
-        Checks if the current gene is greater than another gene
-        Looks to see if the current gene's in_node is greater than the other genes's in node,
-        or the in nodes are equal, and the current gene's out node is greater than the other's out node
-        :param other: An object which should represent a Gene
-        :return: True if the structure of the gene is greater than the structure of the other, False otherwise
+        Checks to see if the score of the current genome is greater than the score of the other
+        :param other: An object which should represent a Genome
+        :return: True if the score of the genome is greater than the score of the other, False otherwise
         """
         if isinstance(other, Genome):
-            return self.in_node > other.in_node or (self.in_node == other.in_node and self.out_node > other.out_node)
+            return self.raw_fitness > other.raw_fitness
         else:
-            raise TypeError("Greater than or equal is not supported between %s and %s" % (str(type(self)), str(type(other))))
+            raise TypeError("Greater than is not supported between %s and %s" % (str(type(self)), str(type(other))))
 
     def __ge__(self, other: Genome) -> bool:
         """
-        Checks if the current gene is greater than or equal to another gene
-        Looks to see if the current gene's in_node is greater than the other genes's in node,
-        or the in nodes are equal, and the current gene's out node is greater than or equal to the other's out node
-        :param other: An object which should represent a Gene
-        :return: True if the structure of the gene is greater than or equal to the structure of the other, False otherwise
+        Checks to see if the score of the current genome is greater than or equal to the score of the other
+        :param other: An object which should represent a Genome
+        :return: True if the score of the genome is greater than or equal to the score of the other, False otherwise
         """
         if isinstance(other, Genome):
-            return self.in_node > other.in_node or (self.in_node == other.in_node and self.out_node >= other.out_node)
+            return self.raw_fitness >= other.raw_fitness
         else:
             raise TypeError("Greater than or equal is not supported between %s and %s" % (str(type(self)), str(type(other))))
-
-    def __hash__(self):
-        """
-        Uses the structure to produce a hash that is unique for each structure
-        :return: a hash that is unique for each structure
-        """
-        hash_agg = 0
-        shift = 1
-        in_node = self.in_node
-        out_node = self.out_node
-        while in_node > 0 or out_node > 0:
-            hash_agg += shift * (in_node % 10) + shift * 10 * (out_node % 10)
-            in_node = in_node // 10
-            out_node = out_node // 10
-            shift *= 100
-        return hash_agg
